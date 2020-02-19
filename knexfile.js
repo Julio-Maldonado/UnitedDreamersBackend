340b4a648c7cf45903cd5f316892e6d688880cd6
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-02-18 22:26:27 -0800

    testing knexfile fix

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
       host: "us-cdbr-iron-east-04.cleardb.net",
       user: "b86c797258618e",
       password: "98367645",
-      database: "united_dreamers_database"
+      // database: "united_dreamers_database"
     }
   }
 }
