7ce943d50d6ddc89628a7b6a61df7b38a909d540
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-02-18 21:52:23 -0800

    added knex config for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,26 @@
 module.exports = {
-  client: 'mysql',
-  connection: {
-    user: 'root',
-    password: 'password',
-    database: 'united_dreamers_database'
+  // client: 'mysql',
+  // connection: {
+  //   user: 'root',
+  //   password: 'password',
+  //   database: 'united_dreamers_database'
+  // }
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: 'password',
+      database: "united_dreamers_database"
+    }
+  },
+  production: {
+    client: "mysql",
+    connection: {
+      host: "us-cdbr-iron-east-04.cleardb.net",
+      user: "b86c797258618e",
+      password: "98367645",
+      database: "united_dreamers_database"
+    }
   }
 }
