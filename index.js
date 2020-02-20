b4ac9a2effc31160d323c124cc1e3fd4d74872c8
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-02-19 21:47:30 -0800

    update

--- index.js
+++ index.js
@@ -40,37 +40,44 @@ function prettyJSON(obj) {
 }
 
 app.post('/createCompany', (req, res) => {
-  console.log(`req = ${prettyJSON(req)}`)
-  console.log(`req.body = ${prettyJSON(req.body)}`)
-  for (key in req.body) {
-    console.log(req.body['key']);
-  }
-  store
-    .createCompany({
-      owner_first_name: req.body.firstName,
-      owner_last_name: req.body.lastName,
-      email_address: req.body.emailAddress,
-      phone_number: req.body.phoneNumber,
-      company_name: req.body.companyName,
-      category: req.body.category,
-      description: req.body.description,
-      services: req.body.services,
-      website_url: req.body.companyURL,
-      facebook_url: req.body.companyFacebookURL,
-      instagram_url: req.body.companyInstagramUsername,
-      twitter_url: req.body.companyTwitterUsername,
-      zip_code: req.body.zipCode,
-      city: req.body.city,
-      state: req.body.state,
-      country: req.body.country,
-      daca_owned: true,
-      support_daca: true,
-      signed_support_for_daca: true,
-      against_daca: false,
-      donated_against_daca: false
+  let body = '';
+
+  req.on('data', (data) => {
+    body = String(data);
+  });
+  req.on('end', () => {
+    console.log(`req = ${prettyJSON(req)}`)
+    console.log(`req.body = ${prettyJSON(req.body)}`)
+    for (key in req.body) {
+      console.log(req.body['key']);
+    }
+    store
+      .createCompany({
+        owner_first_name: req.body.firstName,
+        owner_last_name: req.body.lastName,
+        email_address: req.body.emailAddress,
+        phone_number: req.body.phoneNumber,
+        company_name: req.body.companyName,
+        category: req.body.category,
+        description: req.body.description,
+        services: req.body.services,
+        website_url: req.body.companyURL,
+        facebook_url: req.body.companyFacebookURL,
+        instagram_url: req.body.companyInstagramUsername,
+        twitter_url: req.body.companyTwitterUsername,
+        zip_code: req.body.zipCode,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        daca_owned: true,
+        support_daca: true,
+        signed_support_for_daca: true,
+        against_daca: false,
+        donated_against_daca: false
+      })
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(400))
     })
-    .then(() => res.sendStatus(200))
-    .catch(() => res.sendStatus(400))
 })
 
 app.listen(PORT, () => {
