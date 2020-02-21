351db0409fe8a200eeb8370555adb3480aff8ee9
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-02-20 22:46:42 -0800

    update

--- knexfile.js
+++ knexfile.js
@@ -5,23 +5,31 @@ module.exports = {
   //   password: 'password',
   //   database: 'united_dreamers_database'
   // }
-  client: 'mysql',
-  development: {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: 'password',
-      database: "united_dreamers_database"
-    }
-  },
-  production: {
-    client: "mysql",
-    connection: {
-      host: "us-cdbr-iron-east-04.cleardb.net",
-      user: "b86c797258618e",
-      password: "98367645",
-      database: "heroku_55e2ea1dcf37cd4"
-    }
+  // client: 'mysql',
+  // development: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: "127.0.0.1",
+  //     user: "root",
+  //     password: 'password',
+  //     database: "united_dreamers_database"
+  //   }
+  // },
+  // production: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: "us-cdbr-iron-east-04.cleardb.net",
+  //     user: "b86c797258618e",
+  //     password: "98367645",
+  //     database: "heroku_55e2ea1dcf37cd4"
+  //   }
+  // }
+
+  client: "mysql",
+  connection: {
+    host: "us-cdbr-iron-east-04.cleardb.net",
+    user: "b86c797258618e",
+    password: "98367645",
+    database: "heroku_55e2ea1dcf37cd4"
   }
 }
